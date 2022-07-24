1ff023e236eb7270519b6026ef5a7f0a063089c4
Mohamad Tayeb <m.tayeb993@gmail.com>
2022-07-24 15:01:29 +0300

    complete intermediate react projects

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ut
+        vitae magni labore facilis dolore molestiae numquam quaerat quos,
+        voluptatibus perferendis cupiditate et tempore, temporibus incidunt
+        animi amet similique consequatur.
+      </p>
+    </section>
+  )
+}
+
+export default About
---
+++ 23-quiz/setup/src/SetupForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const SetupForm = () => {
+  return <div className='loading'>setup form</div>
+}
+
+export default SetupForm
